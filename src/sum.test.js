597dffe54d66915a154daf6d17d76af47fbab708
momokuroda <129722941+momokuroda@users.noreply.github.com>
2023-04-14 10:02:36 +0900

    constの練習

--- src/sum.test.js
+++ src/sum.test.js
@@ -3,3 +3,16 @@ const sum = require('./sum');
 test('adds 2 + 2 to equal 4', () => {
   expect(sum(2, 2)).toBe(4);
 });
+
+test(`プログラムコメントの練習`, () => {
+  //エラーになるため、実行させないようにコメントにする。
+  /* expect(sum(1,2)).toBe(4);
+  */
+});
+
+test(`constの練習`, () => {
+  const bookTitle = '本';
+  const bookPrice = 3000;
+  expect(bookTitle).toBe('本');
+  expect(bookPrice).toBe(3000);
+});
