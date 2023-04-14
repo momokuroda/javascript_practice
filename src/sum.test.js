95527871e61f1438d7ae31652ec5540584e10721
momokuroda <129722941+momokuroda@users.noreply.github.com>
2023-04-14 11:52:47 +0900

    console.logの練習

--- src/sum.test.js
+++ src/sum.test.js
@@ -22,11 +22,18 @@ test(`letの練習`, () => {
   let bookTitle ;
   expect(bookTitle).toBe(undefined);
   let bookPrice = 3000;
-  
+
   bookTitle = '本';
   expect(bookTitle).toBe('本');
   expect(bookPrice).toBe(3000);
 
   bookPrice = 5000;
   expect(bookPrice).toBe(5000);
+});
+
+test('console.logの練習', () => {
+  const total = 42+42;
+  total;
+  console.log("total");
+  total;
 });
