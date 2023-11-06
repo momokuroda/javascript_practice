48db64e0db3f90bf355858e354d28c61a0caac59
momokuroda <129722941+momokuroda@users.noreply.github.com>
2023-11-06 13:33:54 +0900

    【非同期処理】直列/並列実行の練習
    練習2として直列実行、並列実行を実装してみています。

--- src/async2.js
+++ src/async2.js
@@ -47,34 +47,34 @@
 
 // 3)失敗する(アクセスできないurlを指定する)
 
-// // 練習2 直列実行と並列実行で実行時間の差を理解する
-// // 直列実行
-// const startTime = Date.now();
-// async function serial() {
-//   const response1 = await fetch("https://google.com");
-//   const response2 = await fetch("https://google.com");
-//   const response3 = await fetch("https://google.com");
-//   const response4 = await fetch("https://google.com");
-//   const response5 = await fetch("https://google.com");
-//   const diffTime = Date.now() - startTime;
-//   console.log(`直列実行 : ${diffTime}`);
-// }
-// serial();
+// 練習2 直列実行と並列実行で実行時間の差を理解する
+// 直列実行
+const startTime = Date.now();
+async function serial() {
+  const response1 = await fetch("https://google.com");
+  const response2 = await fetch("https://google.com");
+  const response3 = await fetch("https://google.com");
+  const response4 = await fetch("https://google.com");
+  const response5 = await fetch("https://google.com");
+  const diffTime = Date.now() - startTime;
+  console.log(`直列実行 : ${diffTime}`);
+}
+serial();
 
-// // 並列実行
-// const startTime2 = Date.now();
-// async function parallel() {
-//   const [response1, response2, response3, response4, response5] =
-//     await Promise.all([
-//       fetch("https://google.com"),
-//       fetch("https://google.com"),
-//       fetch("https://google.com"),
-//       fetch("https://google.com"),
-//       fetch("https://google.com"),
-//     ]);
+// 並列実行
+const startTime2 = Date.now();
+async function parallel() {
+  const [response1, response2, response3, response4, response5] =
+    await Promise.all([
+      fetch("https://google.com"),
+      fetch("https://google.com"),
+      fetch("https://google.com"),
+      fetch("https://google.com"),
+      fetch("https://google.com"),
+    ]);
 
-//   const diffTime = Date.now() - startTime;
-//   console.log(`並列実行 : ${diffTime}`);
-// }
+  const diffTime = Date.now() - startTime;
+  console.log(`並列実行 : ${diffTime}`);
+}
 
-// parallel();
+parallel();
